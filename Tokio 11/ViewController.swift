0d523f3d33dc7f0927d1f789c9b8d3d45b3e9e0f
Osasu Sanachez <osasusan@gmail.com>
2026-04-28 02:03:40 +0200

    Add localization practice (Storyboard + code)
    - Práctica 1: Localización de Label y Button desde el Storyboard
      usando archivos Main.strings para español e inglés
    - Práctica 2: Localización desde código usando NSLocalizedString
      y archivo Localizable.strings para español e inglés

--- Tokio 11/ViewController.swift
+++ Tokio 11/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lbName: UILabel!
+    @IBOutlet weak var lbPrac: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+            // Do any additional setup after loading the view.
+        
+        lbPrac.text = NSLocalizedString("Ejecicio", comment: "")
+        lbName.text = NSLocalizedString("Nombre", comment: "")
     }
-
+    
 
 }
 
